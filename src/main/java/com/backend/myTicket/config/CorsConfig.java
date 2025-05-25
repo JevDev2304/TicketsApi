5c18fae36e88991fa1bbabbb08fccc4a925f60de
Jevdev2304 <jvaldes245@soyudemedellin.edu.co>
2025-05-24 21:28:19 -0500

    I solved a problem related with CORS

--- src/main/java/com/backend/myTicket/config/CorsConfig.java
+++ src/main/java/com/backend/myTicket/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
+                .allowCredentials(false)
                 .maxAge(3600);
     }
 }
